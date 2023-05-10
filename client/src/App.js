27b89a25fa6f2ced16e10e7cc28f31c413dd8b7e
Paulius <pdragunas@gmail.com>
2023-05-09 18:22:13 -0700

    Repo setup: fix submodule issue for client folder

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+const Example = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/example/message')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Example Component</h1>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Example;
